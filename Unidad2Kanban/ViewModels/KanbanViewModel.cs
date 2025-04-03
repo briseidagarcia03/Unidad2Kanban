d7d242d39ab6abe2d83e9ec86ec9353738fe9c0f
briseidagarcia03 <201G0253@rcarbonifera.tecnm.mx>
2025-04-02 22:18:18 -0600

    trae la página

---
+++ Unidad2Kanban/ViewModels/KanbanViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unidad2Kanban.Services;
+
+namespace Unidad2Kanban.ViewModels
+{
+    public class KanbanViewModel
+    {
+        KanbanServer server = new();
+
+        public KanbanViewModel()
+        {
+            server.Iniciar();
+        }
+    }
+}
